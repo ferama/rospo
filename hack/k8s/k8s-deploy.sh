e285e0f6c3d959ffd289e1be10b0b2c2833a88ed
Marco Ferragina <marco.ferragina@gmail.com>
2021-05-21 14:50:19 +0200

    updated k8s example

--- hack/k8s/k8s-deploy.sh
+++ hack/k8s/k8s-deploy.sh
@@ -7,7 +7,6 @@ kubectl create configmap rospo-config \
     --from-file=./id_rsa \
     --from-file=./known_hosts \
     --from-file=./rospo.yaml \
-    --from-file=./server_key \
     --dry-run -o yaml | kubectl apply -f -
 
 kubectl apply -f deployment.yaml
