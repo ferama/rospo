24118757e6ff0852f01153efe0e9e5108b533156
Marco Ferragina <marco.ferragina@gmail.com>
2021-05-22 19:46:45 +0200

    use secret object for relevant data

--- hack/k8s/k8s-deploy.sh
+++ hack/k8s/k8s-deploy.sh
@@ -2,8 +2,11 @@
 
 # take this script as a sample.
 
-kubectl create configmap rospo-config \
+kubectl create secret generic rospo-secret \
     --from-file=./id_rsa \
+    --dry-run=client -o yaml | $kubectl apply -f -
+
+kubectl create configmap rospo-config \
     --from-file=./known_hosts \
     --from-file=./rospo.yaml \
     --dry-run=client -o yaml | kubectl apply -f -
