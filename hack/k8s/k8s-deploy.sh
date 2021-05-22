17b04d4d7a2b7cacafd507dd047401b77aa25b0e
Marco Ferragina <marco.ferragina@gmail.com>
2021-05-22 19:31:13 +0200

    better k8s deployment script example

--- hack/k8s/k8s-deploy.sh
+++ hack/k8s/k8s-deploy.sh
@@ -6,6 +6,7 @@ kubectl create configmap rospo-config \
     --from-file=./id_rsa \
     --from-file=./known_hosts \
     --from-file=./rospo.yaml \
-    --dry-run -o yaml | kubectl apply -f -
+    --dry-run=client -o yaml | kubectl apply -f -
 
 kubectl apply -f deployment.yaml
+kubectl rollout restart deployment/rospo
