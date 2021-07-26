1a5535ccd16a26c970e27794570639f021ddf8fa
Marco Ferragina <marco.ferragina@gmail.com>
2021-07-26 18:20:30 +0200

    enabled cors

--- pkg/web/web.go
+++ pkg/web/web.go
@@ -1,9 +1,12 @@
 package web
 
 import (
+	"time"
+
 	"github.com/ferama/rospo/pkg/sshc"
 	pipeapi "github.com/ferama/rospo/pkg/web/api/pipe"
 	tunapi "github.com/ferama/rospo/pkg/web/api/tun"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,11 +15,20 @@ func StartServer(isDev bool, sshConn *sshc.SshConnection, conf *WebConf) {
 		gin.SetMode(gin.ReleaseMode)
 	}
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	// r.GET("/ping", func(c *gin.Context) {
+	// 	c.JSON(200, gin.H{
+	// 		"message": "pong",
+	// 	})
+	// })
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"*"},
+		AllowHeaders:     []string{"Content-Type, Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	pipeapi.Routes(r.Group("/api/pipes"))
 	tunapi.Routes(sshConn, r.Group("/api/tuns"))
