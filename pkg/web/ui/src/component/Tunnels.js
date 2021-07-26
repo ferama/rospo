4fb81b28dd7f091ec93f0ee2796cec625afd361e
Marco Ferragina <marco.ferragina@gmail.com>
2021-07-26 19:36:46 +0200

    added basic ui skeleton

---
+++ pkg/web/ui/src/component/Tunnels.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class Tunnels extends React.Component {
+
+    render () {
+        return (
+            <div>
+                Tunnels
+            </div>
+        )
+    }
+} 
