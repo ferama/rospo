bd9c6e607a5280522fcfc7fdd40571d4b4e74c66
Marco Ferragina <marco.ferragina@gmail.com>
2021-08-02 10:35:57 +0200

    sshd fix and more unit tests

--- cmd/sshd.go
+++ cmd/sshd.go
@@ -10,7 +10,7 @@ func init() {
 	rootCmd.AddCommand(sshdCmd)
 
 	sshdCmd.Flags().StringP("sshd-authorized-keys", "K", "./authorized_keys", "ssh server authorized keys path")
-	sshdCmd.Flags().StringP("sshd-listen", "P", "2222", "the ssh server listen address")
+	sshdCmd.Flags().StringP("sshd-listen-address", "P", ":2222", "the ssh server listen address")
 	sshdCmd.Flags().StringP("sshd-key", "I", "./server_key", "the ssh server key path")
 	sshdCmd.Flags().BoolP("disable-shell", "D", false, "if set disable shell/exec")
 }
@@ -22,7 +22,7 @@ var sshdCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		sshdKey, _ := cmd.Flags().GetString("sshd-key")
 		sshdAuthorizedKeys, _ := cmd.Flags().GetString("sshd-authorized-keys")
-		sshdListenAddress, _ := cmd.Flags().GetString("sshd-port")
+		sshdListenAddress, _ := cmd.Flags().GetString("sshd-listen-address")
 		disableShell, _ := cmd.Flags().GetBool("disable-shell")
 
 		config := &sshd.SshDConf{
--- pkg/utils/keys_test.go
+++ pkg/utils/keys_test.go
@@ -1,6 +1,13 @@
 package utils
 
-import "testing"
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"testing"
+
+	"golang.org/x/crypto/ssh"
+)
 
 func TestGenerateKeys(t *testing.T) {
 	key, err := GeneratePrivateKey()
@@ -10,8 +17,34 @@ func TestGenerateKeys(t *testing.T) {
 
 	EncodePrivateKeyToPEM(key)
 
-	_, err = GeneratePublicKey(&key.PublicKey)
+	bytes, err := GeneratePublicKey(&key.PublicKey)
 	if err != nil {
 		t.Error(err)
+	}
+	file, err := ioutil.TempFile("", "testkey")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.Remove(file.Name())
+	err = WriteKeyToFile(bytes, file.Name())
+	if err != nil {
+		t.Error(err)
+	}
+
+	file, err = ioutil.TempFile("", "testkey")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.Remove(file.Name())
+	pubkey, _ := ssh.NewPublicKey(&key.PublicKey)
+	AddHostKeyToKnownHosts("testhost:2222", pubkey, file.Name())
+
+	SerializePublicKey(pubkey)
+}
+
+func TestIdentity(t *testing.T) {
+	id := LoadIdentityFile("testdata/identity")
+	if id == nil {
+		t.Fail()
 	}
 }
