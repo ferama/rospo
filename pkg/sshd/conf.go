7b82f7bb5be22fc09922379b103ac56126cf8f07
Marco Ferragina <marco.ferragina@gmail.com>
2021-08-20 21:43:28 +0200

    support password auth on sshd

--- pkg/sshd/conf.go
+++ pkg/sshd/conf.go
@@ -4,6 +4,8 @@ package sshd
 type SshDConf struct {
 	Key                string `yaml:"server_key"`
 	AuthorizedKeysFile string `yaml:"authorized_keys"`
+
+	Password string `yaml:"authorized_password"`
 	// The address the sshd server will listen too
 	ListenAddress string `yaml:"listen_address"`
 	// if true the exec,shell requests will be ignored
