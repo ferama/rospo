f35388921f7437af5574987591d690822a55275a
Marco Ferragina <marco.ferragina@gmail.com>
2022-02-19 22:43:50 +0100

    custom copyBuffer function

--- pkg/utils/helpers.go
+++ pkg/utils/helpers.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"bufio"
+	"errors"
 	"io"
 	"log"
 	"os"
@@ -105,24 +106,102 @@ func GetUserDefaultShell(username string) string {
 	return fallback
 }
 
+// copyBuffer is the actual implementation of Copy and CopyBuffer.
+func copyBuffer(dst io.Writer, src io.Reader, wch chan int64) (err error) {
+	var buf []byte
+	size := 32 * 1024
+	if l, ok := src.(*io.LimitedReader); ok && int64(size) > l.N {
+		if l.N < 1 {
+			size = 1
+		} else {
+			size = int(l.N)
+		}
+	}
+	buf = make([]byte, size)
+	for {
+		nr, er := src.Read(buf)
+		if nr > 0 {
+			nw, ew := dst.Write(buf[0:nr])
+			if nw < 0 || nr < nw {
+				nw = 0
+				if ew == nil {
+					ew = errors.New("invalid write result")
+				}
+			}
+			// written += int64(nw)
+			select {
+			case wch <- int64(nw):
+			default:
+			}
+			if ew != nil {
+				err = ew
+				break
+			}
+			if nr != nw {
+				err = io.ErrShortWrite
+				break
+			}
+		}
+		if er != nil {
+			if er != io.EOF {
+				err = er
+			}
+			break
+		}
+	}
+	return err
+}
+
 // CopyConnWithOnClose copy packets from c1 to c2 and viceversa. Calls the onClose function
 // when the connection is interrupted
-func CopyConnWithOnClose(c1 io.ReadWriteCloser, c2 io.ReadWriteCloser, onClose func()) {
+func CopyConnWithOnClose(
+	c1 io.ReadWriteCloser,
+	c2 io.ReadWriteCloser,
+	onClose func()) chan int64 {
+
+	w1 := make(chan int64)
+	w2 := make(chan int64)
+	throughput := make(chan int64)
+
 	var once sync.Once
-	close := func() {
+	connClose := func() {
 		c1.Close()
 		c2.Close()
+		close(w1)
+		close(w2)
+		close(throughput)
 		onClose()
 	}
+
 	go func() {
-		io.Copy(c1, c2)
-		once.Do(close)
+		for nw := range w1 {
+			select {
+			case throughput <- int64(nw):
+			default:
+			}
+		}
 	}()
 
 	go func() {
-		io.Copy(c2, c1)
-		once.Do(close)
+		for nw := range w1 {
+			select {
+			case throughput <- int64(nw):
+			default:
+			}
+		}
 	}()
+
+	go func() {
+		copyBuffer(c1, c2, w1)
+		once.Do(connClose)
+	}()
+
+	go func() {
+		copyBuffer(c2, c1, w2)
+		once.Do(connClose)
+	}()
+
+	return throughput
 }
 
 // CopyConn copy packets from c1 to c2 and viceversa
