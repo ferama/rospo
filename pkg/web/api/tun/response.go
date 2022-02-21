2f09db594cbbd4feba7a677a29af65a29aac8fe2
Marco Ferragina <marco.ferragina@gmail.com>
2022-02-21 09:36:14 +0100

    bring tun throughput stats to the ui

--- pkg/web/api/tun/response.go
+++ pkg/web/api/tun/response.go
@@ -7,10 +7,12 @@ import (
 )
 
 type tunResponseItem struct {
-	ID              int            `json:"Id"`
-	Listener        net.Addr       `json:"Listener"`
-	IsListenerLocal bool           `json:"IsListenerLocal"`
-	Endpoint        utils.Endpoint `json:"Endpoint"`
-	ClientsCount    int            `json:"ClientsCount"`
-	IsStoppable     bool           `json:"IsStoppable"`
+	ID               int            `json:"Id"`
+	Listener         net.Addr       `json:"Listener"`
+	IsListenerLocal  bool           `json:"IsListenerLocal"`
+	Endpoint         utils.Endpoint `json:"Endpoint"`
+	ClientsCount     int            `json:"ClientsCount"`
+	IsStoppable      bool           `json:"IsStoppable"`
+	Throughput       int64          `json:"Throughput"`
+	ThroughputString string         `json:"ThroughputString"`
 }
