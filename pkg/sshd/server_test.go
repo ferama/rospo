c9552192404e7ad50ff0e4d59f148bff06a02d8f
Marco Ferragina <marco.ferragina@gmail.com>
2022-08-02 10:40:44 +0200

    test identity generation

--- pkg/sshd/server_test.go
+++ pkg/sshd/server_test.go
@@ -1,8 +1,11 @@
 package sshd
 
 import (
+	"errors"
 	"fmt"
 	"net"
+	"os"
+	"path/filepath"
 	"strings"
 	"testing"
 	"time"
@@ -14,6 +17,37 @@ import (
 func getPort(addr net.Addr) string {
 	parts := strings.Split(addr.String(), ":")
 	return parts[1]
+}
+
+func TestNoIdentity(t *testing.T) {
+	tmpDir := t.TempDir()
+
+	tmpPath := filepath.Join(tmpDir, "notexisting")
+	// start a local sshd
+	serverConf := &SshDConf{
+		Key:               tmpPath,
+		AuthorizedKeysURI: []string{tmpPath},
+		ListenAddress:     "127.0.0.1:0",
+		DisableShell:      false,
+		DisableAuth:       true,
+	}
+	sd := NewSshServer(serverConf)
+	defer func() {
+		os.Remove(tmpPath)
+		os.Remove(filepath.Join(tmpDir, "notexisting.pub"))
+	}()
+	go sd.Start()
+	var addr net.Addr
+	for {
+		addr = sd.GetListenerAddr()
+		if addr != nil {
+			break
+		}
+		time.Sleep(500 * time.Millisecond)
+	}
+	if _, err := os.Stat(tmpPath); errors.Is(err, os.ErrNotExist) {
+		t.Fatal(err)
+	}
 }
 
 func TestSftpSubsystem(t *testing.T) {
