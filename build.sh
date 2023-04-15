66f5b1ac190339ff14fe9a579c40f93c898bddb2
Marco Ferragina <marco.ferragina@gmail.com>
2023-04-15 22:42:54 +0200

    build: disable cgo

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ build() {
     EXT=""
     [[ $GOOS = "windows" ]] && EXT=".exe"
     echo "Building ${GOOS} ${GOARCH}"
-    go build \
+    CGO_ENABLED=0 go build \
         -trimpath \
         -ldflags="-s -w -X 'github.com/ferama/rospo/cmd.Version=$VERSION'" \
         -o ./bin/rospo-${GOOS}-${GOARCH}${EXT} .
