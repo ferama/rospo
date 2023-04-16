1df6d95d2c1a7032c4e68691e9050db9c5db256f
Marco Ferragina <marco.ferragina@gmail.com>
2023-04-16 19:55:05 +0200

    implemented socks proxy

---
+++ cmd/proxy.go
@@ -0,0 +1,37 @@
+package cmd
+
+import (
+	"log"
+
+	"github.com/ferama/rospo/cmd/cmnflags"
+	"github.com/ferama/rospo/pkg/sshc"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(proxyCmd)
+	// sshc options
+	cmnflags.AddSshClientFlags(proxyCmd.Flags())
+
+	proxyCmd.Flags().StringP("listen-address", "l", "127.0.0.1:1337", "the socks proxy listener address")
+}
+
+var proxyCmd = &cobra.Command{
+	Use:   "proxy [user@]host[:port]",
+	Short: "Starts a socks proxy",
+	Long:  "Starts a socks proxy",
+	Args:  cobra.MinimumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		sshcConf := cmnflags.GetSshClientConf(cmd, args[0])
+		conn := sshc.NewSshConnection(sshcConf)
+		go conn.Start()
+
+		listenAddress, _ := cmd.Flags().GetString("listen-address")
+
+		sockProxy := sshc.NewSockProxy(conn)
+		err := sockProxy.Start(listenAddress)
+		if err != nil {
+			log.Fatalln(err)
+		}
+	},
+}
---
+++ pkg/sshc/sock_proxy.go
@@ -0,0 +1,42 @@
+package sshc
+
+import (
+	"context"
+	"net"
+
+	"github.com/armon/go-socks5"
+)
+
+type SocksProxy struct {
+	sshConn *SshConnection
+}
+
+func NewSockProxy(sshConn *SshConnection) *SocksProxy {
+	p := &SocksProxy{
+		sshConn: sshConn,
+	}
+
+	return p
+}
+
+// Start starts the local socks proxy
+func (p *SocksProxy) Start(socks5Address string) error {
+	p.sshConn.Connected.Wait()
+
+	conf := &socks5.Config{
+		Dial: func(ctx context.Context, network, addr string) (net.Conn, error) {
+			return p.sshConn.Client.Dial(network, addr)
+		},
+	}
+
+	serverSocks, err := socks5.New(conf)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("local sock5 proxy listening at '%s'", socks5Address)
+	if err := serverSocks.ListenAndServe("tcp", socks5Address); err != nil {
+		return err
+	}
+	return nil
+}
