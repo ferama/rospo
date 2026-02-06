a359603a9b96bddb3063b1028ffd5be0f86ad78d
Marco Ferragina <marco.ferragina@gmail.com>
2026-02-06 14:53:08 +0100

    updated vpn script

--- hack/poor-vpn-macos.sh
+++ hack/poor-vpn-macos.sh
@@ -17,14 +17,23 @@ SSH_PORT="[put your ssh port here]"
 REMOTE_DNS="1.1.1.1:53"
 
 ############################################################################
-GATEWAY=`route -n get default | grep gateway | awk '{print $2}'`
-GATEWAY6=`route -n get -inet6 default | grep gateway | awk '{print $2}'`
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root"
+    exit
+fi
+
+ROSPO=$(which rospo)
+USER=$(logname)
+GATEWAY=$(route -n get default | grep gateway | awk '{print $2}')
+GATEWAY6=$(route -n get -inet6 default  2>&1 > /dev/null | grep gateway | awk '{print $2}')
 TMPFILE=$(mktemp -p /tmp)
 
 egress() {
-    sudo route delete $SSH_HOST
-    sudo route add default $GATEWAY
-    sudo route add -inet6 default $GATEWAY6
+    route delete $SSH_HOST
+    route add default $GATEWAY
+    if [ -n "$GATEWAY6" ]; then
+        route add -inet6 default $GATEWAY6
+    fi
     networksetup -setdnsservers Wi-Fi empty
     rm $TMPFILE
 }
@@ -32,9 +41,10 @@ trap egress EXIT
 
 run_rospo() {
 
-    cat > $TMPFILE <<EOF
+    cat >$TMPFILE <<EOF
 sshclient:
   server: $SSH_HOST:$SSH_PORT
+  insecure: true
 
 socksproxy:
   listen_address: :1080
@@ -43,33 +53,34 @@ dnsproxy:
   listen_address: :53
   remote_dns_address: $REMOTE_DNS
 EOF
-    
-    rospo run $TMPFILE &
+    chown $USER $TMPFILE
+    sudo -u $USER $ROSPO run $TMPFILE &
 }
 
 run_tun() {
     echo "Starting tun2socks..."
-    sudo tun2socks \
+
+    tun2socks \
         -device utun123 \
         -proxy socks5://127.0.0.1:1080 \
         -interface en0 \
+        -loglevel error \
         -tcp-auto-tuning &
 
-
-    until ifconfig | grep utun123;
-    do
+    until ifconfig | grep utun123; do
         sleep 1
         echo "working..."
     done
 
-    sudo ifconfig utun123 198.18.0.1 198.18.0.1 up
+    ifconfig utun123 198.18.0.1 198.18.0.1 up
 
-    sudo route delete default
-    sudo route delete -inet6 default
+    route delete default
+    if [ -n "$GATEWAY6" ]; then
+        route delete -inet6 default
+    fi
 
-
-    sudo route add default -interface utun123
-    sudo route add $SSH_HOST $GATEWAY
+    route add default -interface utun123
+    route add $SSH_HOST $GATEWAY
 
     networksetup -setdnsservers Wi-Fi 127.0.0.1
 }
